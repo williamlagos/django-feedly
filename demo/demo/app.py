f0a96d0f8750622b3e4eecf3140f5beaf049a40f
William Oliveira de Lagos <william.lagos@live.com>
2014-03-03 00:03:18 -0300

    Reorganized and dried the structure of demo

--- demo/demo/app.py
+++ demo/demo/app.py
@@ -7,13 +7,146 @@ from django.http import HttpResponseRedirect as redirect
 from django.conf import settings
 
 from demo.models import Spreadable,Image,Playable,Spreaded,Product
-from main import Efforia
-from feedly.feed import Activity
-
-from main import Efforia
 from feedly.feed import Activity
 from feedly.models import Profile,Sellable
 from demo.models import Event,Project,Interest,Movement,Promoted
+
+class Efforia(Mosaic):
+    def __init__(self): pass
+    def verify_permissions(self,request):
+        perm = 'super'
+        if 'permissions' in request.COOKIES:
+            perm = request.COOKIES['permissions']
+        permissions = True if 'super' in perm else False
+        return permissions
+    def start(self,request):
+        # Painel do usuario
+        u = user('efforia'); 
+        permissions = self.verify_permissions(request)
+        actions = settings.EFFORIA_ACTIONS; apps = []
+        for a in settings.EFFORIA_APPS: apps.append(actions[a])
+        return render(request,'interface.html',{'static_url':settings.STATIC_URL,
+                                            'user':user('efforia'),'perm':permissions,
+                                            'name':'%s %s' % (u.first_name,u.last_name),'apps':apps
+                                            },content_type='text/html')
+        # Pagina inicial
+        #p = list(Page.objects.filter(user=superuser()))
+        #return render(request,'index.html',{'static_url':settings.STATIC_URL},content_type='text/html')
+    def external(self,request):
+        u = self.current_user(request)
+        sellables = Sellable.objects.filter(user=u)
+        for s in sellables: s.paid = True
+        return self.redirect('/')
+    def profile_view(self,request,name):
+        if len(list(User.objects.filter(username=name))) > 0: request.session['user'] = name
+        r = redirect('/')
+        r.set_cookie('permissions','view_only')
+        return r
+    def json_decode(self,string):
+        j = json.loads(string,'utf-8')
+        return ast.literal_eval(j)
+    def url_request(self,url,data=None,headers={}):
+        request = urllib2.Request(url=url,data=data,headers=headers)
+        request_open = urllib2.urlopen(request)
+        return request_open.geturl()
+    def do_request(self,url,data=None,headers={}):
+        response = ''
+        request = urllib2.Request(url=url,data=data,headers=headers)
+        try:
+            request_open = urllib2.urlopen(request)
+            response = request_open.read()
+            request_open.close()
+        except urllib2.HTTPError,e:
+            print url
+            print data
+            print headers
+            print e.code
+            print e.msg
+            print e.hdrs
+            print e.fp
+        return response
+    def oauth_post_request(self,url,tokens,data={},social='twitter',headers={}):
+        pass
+        #=======================================================================
+        # #api = json.load(open('settings.json','r'))['social']
+        # posturl ='%s%s'%(api[social]['url'],url)
+        # if 'facebook' in social:
+        #     socialurl = '%s?%s'%(posturl,urllib.urlencode({'access_token':tokens}))
+        #     if 'start_time' in data: data['start_time'] = data['start_time'].date()
+        #     return self.do_request(socialurl,urllib.urlencode(data),headers)
+        # else:
+        #     access_token,access_token_secret = tokens.split(';')
+        #     token = oauth.Token(access_token,access_token_secret)
+        #     consumer_key = api[social]['client_key']
+        #     consumer_secret = api[social]['client_secret']
+        #     consumer = oauth.Consumer(consumer_key,consumer_secret)
+        #     client = oauth.Client(consumer,token)
+        #     try:
+        #         return client.request(posturl,'POST',urllib.urlencode(data))
+        #     except urllib2.HTTPError,e:
+        #         print e.code
+        #         print e.msg
+        #         print e.hdrs
+        #         print e.fp
+        #         return 1
+        #=======================================================================
+    def refresh_google_token(self,token):
+        pass
+        #=======================================================================
+        # api = json.load(open('settings.json','r'))['social']['google']
+        # if not token: token = self.own_access()['google_token']
+        # data = urllib.urlencode({
+        #     'client_id':      api['client_id'],
+        #     'client_secret':  api['client_secret'],
+        #     'refresh_token':  token,
+        #     'grant_type':    'refresh_token' })
+        # return json.loads(self.do_request(api['oauth2_token_url'],data))['access_token']
+        #=======================================================================
+    def object_token(self,token):
+        relations = settings.EFFORIA_TOKENS
+        typobject = relations[token]
+        return typobject
+    def object_byid(self,token,ident):
+        obj = self.object_token(token)
+        return globals()[obj].objects.filter(id=ident)[0]
+    def convert_datetime(self,date_value):
+        d = time.strptime(date_value,'%d/%m/%Y')
+        return datetime.fromtimestamp(time.mktime(d))
+    def authenticate(self,username,password):
+        exists = User.objects.filter(username=username)
+        if exists:
+            if exists[0].check_password(password): 
+                return exists
+        else: return None
+    def authenticated(self):
+        name = self.get_current_user()
+        if not name: 
+            #self.redirect('login')
+            self.render('templates/enter.html',STATIC_URL=settings.STATIC_URL)
+            return False
+        else:
+            return True
+    def accumulate_points(self,points,request=None):
+        if request is None: u = self.current_user()
+        else: u = self.current_user(request)
+        current_profile = Profile.objects.all().filter(user=u)[0]
+        current_profile.points += points
+        current_profile.save()
+    def own_access(self):
+        pass
+        #=======================================================================
+        # objs = json.load(open('settings.json','r'))
+        # google_api = objs['social']['google']
+        # twitter_api = objs['social']['twitter']
+        # facebook_api = objs['social']['facebook']
+        # access = {
+        #     'google_token': google_api['client_token'],
+        #     'twitter_token': '%s;%s' % (twitter_api['client_token'],twitter_api['client_token_secret']),
+        #     'facebook_token': facebook_api['client_token']
+        # }
+        # return access
+        #=======================================================================
+
 
 def sp(x): return '!!' in x[1]
 def pl(x): return '>!' in x[1]
@@ -42,88 +175,6 @@ class Application(Activity):
             elif 'Image' in o: e = filter(im,exclude)
             excludes = [x[0] for x in e]
             feed.extend(objects.exclude(id__in=excludes))
-        
-class Images(Efforia):
-    def __init__(self): pass
-    def view_image(self,request):
-        return render(request,'image.jade',{'static_url':settings.STATIC_URL},content_type='text/html')
-    def upload_image(self,request):
-        photo = request.FILES['Filedata'].read()
-        dropbox = Dropbox()
-        link = dropbox.upload_and_share(photo)
-        res = self.url_request(link)
-        url = '%s?dl=1' % res
-        return url
-    def create_image(self,request):
-        u = self.current_user(request)
-        if 'description' in request.POST:
-            image = list(Image.objects.filter(user=u))[-1:][0]
-            descr = request.POST['description']
-            image.description = descr
-            image.save()
-            return response('Description added to image successfully')
-        i = Image(link=self.upload_image(request),user=u)
-        i.save()
-        return response('Image created successfully')
-
-class Spreads(Efforia):
-    def __init__(self): pass
-    def start_spreadapp(self,request):
-        return render(request,'spreadapp.jade',{'static_url':settings.STATIC_URL},content_type='text/html')
-    def view_spread(self,request):
-        return render(request,"spread.jade",{},content_type='text/html')
-    def create_spread(self,request):
-        u = self.current_user(request)
-        name = u.first_name.lower()
-        text = unicode('%s' % (request.POST['content']))
-        post = Spreadable(user=u,content=text,name='!'+name)
-        post.save()
-        self.accumulate_points(1,request)
-        return response('Spreadable created successfully')
-    
-class Uploads(Efforia):
-    def __init__(self): pass
-    def view_upload(self,request):
-        return render(request,'content.jade',{'static_url':settings.STATIC_URL},content_type='text/html')
-    def set_thumbnail(self,request):
-        u = self.current_user(request)
-        service = StreamService()
-        token = request.GET['id']
-        access_token = u.profile.google_token
-        thumbnail = service.video_thumbnail(token,access_token)
-        play = Playable.objects.filter(user=u).latest('date')
-        play.visual = thumbnail
-        play.token = token
-        play.save()
-        self.accumulate_points(1,request)
-        r = redirect('/')
-        r.set_cookie('token',token)
-        return r
-    def view_content(self,request):
-        u = self.current_user(request)
-        content = title = ''
-        for k,v in request.REQUEST.iteritems(): 
-            if 'title' in k: title = v
-            elif 'content' in k: content = v
-            elif 'status' in k: 
-                return self.set_thumbnail(request)
-        try: 
-            url,token = self.parse_upload(request,title,content)
-            return render(request,'video.jade',{'static_url':settings.STATIC_URL,
-                                                  'hostname':request.get_host(),
-                                                  'url':url,'token':token},content_type='text/html')
-        except Exception: return response('Invalid file for uploading') 
-    def parse_upload(self,request,title,content):
-        keys = ','; keywords = content.split(' ')
-        for k in keywords: k = normalize('NFKD',k.decode('utf-8')).encode('ASCII','ignore')
-        keys = keys.join(keywords)
-        playable = Playable(user=self.current_user(request),name='>'+title,description=content)
-        playable.save()
-        service = StreamService()
-        access_token = self.current_user(request).profile.google_token
-        return service.video_entry(title,content,keys,access_token)
-    def media_chooser(self,request):
-        return render(request,'chooser.jade')
 
 def ca(x): return '@#' in x[1]
 def ev(x): return '@!' in x[1]
--- demo/promote.py
+++ demo/promote.py
@@ -9,6 +9,86 @@ from django.conf import settings
 from main import Efforia
 from demo.models import Sellable,Promoted,Interest,Movement,Project
 
+class Promoteds(Efforia):
+    def promote_form(self,request):
+        return render(request,'promote.jade',{},content_type='text/html')
+    def promote(self,request):
+        u = self.current_user(request)
+        for k,v in request.REQUEST.iteritems():
+            if 'content' in k: content = v
+            elif 'id' in k: promotedid = v
+            elif 'token' in k: token = v
+        move = Movement.objects.filter(cause_id=promotedid)
+        if len(move) > 0: p = Promoted(name='$#',prom=promotedid,content=content,user=u); p.save()
+        else: s = Promoted(name=token,prom=promotedid,content=content,user=u); s.save() 
+        mod,pobj = settings.EFFORIA_TOKENS[token].split('.')
+        o = globals()[pobj].objects.filter(id=promotedid)[0]
+        return response(o.event_id)
+    def promoted(self,request):
+        u = self.current_user(request)
+        ident = request.REQUEST['id']
+        p = Promoted.objects.filter(prom=ident)
+        mod,pobj = settings.EFFORIA_TOKENS[p[0].name].split('.')
+        o = globals()[pobj].objects.filter(id=ident)[0]
+        feed = list(p); feed.append(o)
+        return self.view_mosaic(request,feed)
+
+class Events(Efforia):
+    def __init__(self): pass
+    def event_form(self,request):
+        return render(request,'event.jade',{},content_type='text/html')
+    def view_event(self,request,eventid):
+        e = Event.objects.filter(id=eventid)[0]
+        t = Sellable.objects.filter(sellid=eventid)
+        sum = ratio = count = 0
+        if len(t) > 0:
+            for ticket in t: 
+                if ticket.paid: sum += ticket.value; count += 1
+            ratio = (float(sum)/float(e.value))*100.0
+        permissions = self.verify_permissions(request)
+        return render(request,'event.html',{
+                       'perm':permissions,
+                       'title':e.name[2:],
+                       'ratio':ratio,
+                       'buyers':count,
+                       'event':e,
+                       'static_url':settings.STATIC_URL
+                       },content_type='text/html')
+    def create_event(self,request):
+        u = self.current_user(request)
+        title = descr = local = max = min = value = dates = ''
+        for k,v in request.REQUEST.iteritems():
+            if 'name' in k: title = v
+            elif 'description' in k: descr = v
+            elif 'location' in k: local = v
+            elif 'deadline' in k: dates = v
+            elif 'min' in k: min = v
+            elif 'value' in k: value = v#+int(float(v)*0.1)
+        date = self.convert_datetime(dates)
+        Event(name='@@%s'%title,user=u,deadline=date,description=descr,
+        min=min,value=value,location=local).save()
+        return render(request,'eventimage.jade',{'static_url':settings.STATIC_URL})
+    def event_image(self,request):
+        u = self.current_user(request)
+        photo = request.FILES['Filedata'].read()
+        dropbox = Dropbox()
+        link = dropbox.upload_and_share(photo)
+        res = self.url_request(link)
+        e = Event.objects.filter(user=u).latest('date')
+        e.visual = '%s?dl=1' % res
+        e.save()
+        return response(e.visual)
+    def show_enroll(self,request):
+        u = self.current_user(request)
+        event = Event.objects.filter(user=u,id=request.REQUEST['id'])[0]
+        return render(request,'enroll.jade',{'static_url':settings.STATIC_URL,'value':event.value})
+    def event_id(self,request):
+        u = self.current_user(request)
+        e = list(Event.objects.filter(user=u))[0]
+        e.event_id = request.REQUEST['id']
+        e.save()
+        return response('Event created successfully')
+
 class Projects(Efforia):
     def __init__(self): pass
     def start_promoteapp(self, request):
