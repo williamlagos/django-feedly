bb335af450a5f58d1e5edf038dae5a2a4c8203af
William Oliveira de Lagos <william.lagos@live.com>
2014-03-03 14:44:59 -0300

    Removed dependencies on admin file

--- feedly/admin.py
+++ feedly/admin.py
@@ -21,9 +21,4 @@ from django.contrib import admin
 from models import *
 
 admin.site.register(Profile)
-admin.site.register(Place)
 admin.site.register(Followed)
-admin.site.register(Page)
-admin.site.register(Basket)
-admin.site.register(Sellable)
-admin.site.register(Deliverable)
