0f634b85293376b0288ae3a61648153fac4b09a7
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-03 22:47:02 -0300

    Added initial django tests body for feature validation

--- feedly/tests.py
+++ feedly/tests.py
@@ -17,3 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Feedly. If not, see <http://www.gnu.org/licenses/>.
 #
+
+from django.test import TestCase
+
+# Create your tests here.
